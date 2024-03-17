ee267aaa58b40919b12e67d86c1a0980db93d881
Jau2002 <idamendjaume@gmail.com>
2024-03-17 15:39:01 -0500

    feat: class for valid team

--- src/co/edu/unbosque/Model/Equipo.java
+++
@@ -1,5 +0,0 @@
-package co.edu.unbosque.Model;
-
-public class Equipo {
-
-}
---
+++ src/co/edu/unbosque/Model/Team.java
@@ -0,0 +1,36 @@
+package co.edu.unbosque.Model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Team {
+    private String name;
+    private int star;
+
+    public Team() {
+        name = "unknown";
+        star = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setName(String name) {
+        Pattern format = Pattern.compile("[a-zA-Z]+");
+        Matcher valid = format.matcher(name);
+        if (valid.find()) this.name = name;
+        else System.out.println("Formato no valido");
+    }
+
+    public void setStar(int star) {
+        Pattern format = Pattern.compile("[0-9]+");
+        Matcher valid = format.matcher(String.valueOf(star));
+        if (valid.find()) this.star = star;
+        else System.out.println("Formato no valido");
+    }
+}
